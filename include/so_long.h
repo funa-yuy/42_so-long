46f8068f7e2679c69ffdc5cefb6fd0c6a3c4ecc9
funa <orz.home35@gmail.com>
2025-01-11 18:35:54 +0900

    🛠️ Include/so_long.h

--- include/so_long.h
+++ include/so_long.h
@@ -6,27 +6,32 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:13:57 by miyuu             #+#    #+#             */
-/*   Updated: 2025/01/09 16:37:41 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/01/10 01:33:31 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-# define T_SIZE 32
+#include <mlx.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <X11/keysym.h>
+#include <X11/X.h>
+#include "./lib/get_next_line/get_next_line.h"
 
-typedef struct s_point{
+# define T_SIZE	32
+
+typedef struct s_player
+{
 	int			x;
 	int			y;
-	void		*game_img;
-}			t_point;
+}			t_player;
 
-typedef struct s_img{
-	// char	**repo;
-	// char	**fill;
-	// int		empty;//何もないとこの数
-	// int		wall;//数
-	// int		collects;//数
-	// int		exit;//数
-	// int		player;//数
+typedef struct s_img
+{
 	void	*em_img;
 	void	*wall_img;
 	void	*col_img;
@@ -34,19 +39,16 @@ typedef struct s_img{
 	void	*p_img;
 }				t_img;
 
-typedef struct	s_data {
-	void	*mlx;
-	void	*win;
-	t_img	*img;
-	int		fd;
-	int		y_column;//列(縦軸)
-	int		x_row;//行(横軸)
-	// char	*addr;
-	// int		bits_per_pixel;
-	// int		line_length;
-	// int		endian;
-	t_point	p[1000][1000];
-	t_point player;
+typedef struct s_dat
+{
+	void		*mlx;
+	void		*win;
+	t_img		*img;
+	int			fd;
+	int			y_column;//列(縦軸)
+	int			x_row;//行(横軸)
+	void		*p[1000][1000];
+	t_player	player;
 }				t_data;
 
-
+#endif
