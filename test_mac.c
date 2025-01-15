fd3d7b3b2a0af0516d6b8744d7c5c336e0cbd769
funa <orz.home35@gmail.com>
2025-01-15 16:01:06 +0900

    🚧 幅優先探索をmacでできるようにした：収集物もチェックできるようにした

--- test_mac.c
+++ test_mac.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 01:59:33 by miyuu             #+#    #+#             */
-/*   Updated: 2025/01/15 03:22:18 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/01/15 15:34:05 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,12 +75,13 @@ typedef struct	s_data {
 #define PATH '0' /* 通路 */
 #define WALL '1' /* 壁 */
 #define GOAL 'E' /* ゴール */
+#define COLLECTS 'C' /* ゴール */
 #define PASSED '3' /* 通過したマス */
 
 /* 管理するデータの上限個数 */
-#define MAX_NUM (MEIRO_WIDTH * MEIRO_HEIGHT)
+// #define MAX_NUM (MEIRO_WIDTH * MEIRO_HEIGHT)
 
-#define QUEUE_SIZE (MAX_NUM + 1)
+// #define QUEUE_SIZE (MAX_NUM + 1)
 
 /* マスの位置情報 */
 typedef struct POS {
@@ -95,42 +96,50 @@ typedef struct QUEUE {
 	 /* データの先頭 */
 	 int head;
 	 /* キューされているデータ */
-	 POS_T data[QUEUE_SIZE];
-} QUEUE_T;
-
-QUEUE_T queue;
-
+	 POS_T *data;
+	 int size;
+}			QUEUE_T;
 
 /* キューを初期化する関数 */
-void initQueue(QUEUE_T *queue){
+void initQueue(QUEUE_T *queue, int width, int height){
 
 	/* キューを空に設定 */
 	queue->head = 0;
 	queue->tail = -1;
+
+	/* キューのサイズを設定 */
+	queue->size = (width * height) + 1;
+
+	/* 必要なメモリを確保 */
+	queue->data = (POS_T *)malloc(queue->size * sizeof(POS_T));
+	if (!queue->data) {
+		fprintf(stderr, "メモリ確保に失敗しました\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 /* ENQUEUEする関数 */
-void enqueue(QUEUE_T *queue, POS_T *input){
+void enqueue(QUEUE_T *queue, POS_T *input, int queue_size){
 
 	/* キューが満杯なら何もせず関数終了 */
-	if((queue->tail + 2) % QUEUE_SIZE == queue->head){
+	if((queue->tail + 2) % queue_size == queue->head){
 		printf("キューが満杯でENQUEUEできません\n");
 		return;
 	}
 
 	/* データをデータの最後尾の１つ後ろに格納 */
-	queue->data[(queue->tail + 1) % QUEUE_SIZE] = *input;
+	queue->data[(queue->tail + 1) % queue_size] = *input;
 
 	/* データの最後尾を１つ後ろに移動 */
-	queue->tail = (queue->tail + 1) % QUEUE_SIZE;
+	queue->tail = (queue->tail + 1) % queue_size;
 }
 
 /* DEQUEUEする関数 */
-POS_T *dequeue(QUEUE_T *queue){
+POS_T *dequeue(QUEUE_T *queue, int queue_size){
 	POS_T *ret;
 
 	/* キューが空なら何もせずに関数終了 */
-	if((queue->tail + 1) % QUEUE_SIZE == queue->head){
+	if((queue->tail + 1) % queue_size == queue->head){
 		printf("キューが空です\n");
 		return NULL;
 	}
@@ -139,7 +148,7 @@ POS_T *dequeue(QUEUE_T *queue){
 	ret = &(queue->data[queue->head]);
 
 	/* データの先頭を１つ後ろにずらす */
-	queue->head = (queue->head + 1) % QUEUE_SIZE;
+	queue->head = (queue->head + 1) % queue_size;
 
 	/* 取得したデータを返却 */
 	return ret;
@@ -149,7 +158,7 @@ POS_T *dequeue(QUEUE_T *queue){
 bool check(t_data *data, char **maze, int x_i, int y_j) {
 	// bool ret = true;
 
-	if (x_i < 0 || x_i >= data->player.x || y_j < 0 || y_j >= data->player.y) {
+	if (x_i < 0 || x_i >= data->x_row || y_j < 0 || y_j >= data->y_column) {
 		/* (i,y_j) は迷路外なので通過不可 */
 		// ret = false;
 		return (false);
@@ -173,24 +182,30 @@ bool check(t_data *data, char **maze, int x_i, int y_j) {
 
 
 /* スタート(i,j)からゴールを探索する関数*/
-bool search(t_data *data, char **maze, int s_i, int s_j) {
+bool search(t_data *data, QUEUE_T *queue, char **maze, int s_i, int s_j)
+{
 
 	POS_T pos;
 	bool ret;
+	bool	goal = false;
+	int collect = data->coll_con;
+
+	int	queue_size = (data->x_row * data->y_column) + 1;
+
 
 	/* 次の探索候補としてスタートのマスの情報をキューに格納 */
 	if (check(data, maze, s_i, s_j)) {
 		pos.i = s_i; /* スタートのi座標*/
 		pos.j = s_j; /* スタートのj座標*/
 
-		enqueue(&queue, &pos); /* キューにマスの情報を格納*/
+		enqueue(queue, &pos, queue_size); /* キューにマスの情報を格納*/
 	}
 
 	while (true) {
 		int x_i, y_j;
 
 		/* キューから次の探索候補のマスを取得*/
-		POS_T *next = dequeue(&queue);
+		POS_T *next = dequeue(queue, queue_size);
 		if (next == NULL) {
 			/* 次の探索候補がない場合は探索失敗 */
 			ret = false;
@@ -203,8 +218,24 @@ bool search(t_data *data, char **maze, int s_i, int s_j) {
 		if (maze[y_j][x_i] == GOAL) {
 			/* 探索対象が見つかったので探索成功 */
 			printf("ゴールに到着\n");
-			ret = true;
-			break;
+			goal = true;
+			// break;
+			if (collect == 0 && goal == true)
+			{
+				ret = true;
+				break;
+			}
+		}
+		if (maze[y_j][x_i] != PASSED && maze[y_j][x_i] == COLLECTS) {
+			/* 探索対象が見つかったので探索成功 */
+			// printf("ゴールに到着\n");
+			// ret = true;
+			collect--;
+			if (collect == 0 && goal == true)
+			{
+				ret = true;
+				break;
+			}
 		}
 
 		/* 探索済みのマスに印をつける */
@@ -218,7 +249,7 @@ bool search(t_data *data, char **maze, int s_i, int s_j) {
 			pos.j = y_j - 1; /* 上方向のマスのy_j座標 */
 
 			/* 上方向のマスを探索候補としてキューに格納*/
-			enqueue(&queue, &pos);
+			enqueue(queue, &pos, queue_size);
 		}
 
 		if (check(data, maze, x_i, y_j + 1)) {
@@ -228,7 +259,7 @@ bool search(t_data *data, char **maze, int s_i, int s_j) {
 			pos.j = y_j + 1; /* 下方向のマスのy_j座標 */
 
 			/* 下方向のマスを探索候補としてキューに格納*/
-			enqueue(&queue, &pos);
+			enqueue(queue, &pos, queue_size);
 		}
 
 		if (check(data, maze, x_i - 1, y_j)) {
@@ -238,7 +269,7 @@ bool search(t_data *data, char **maze, int s_i, int s_j) {
 			pos.j = y_j; /* 左方向のマスのy_j座標 */
 
 			/* 左方向のマスを探索候補としてキューに格納*/
-			enqueue(&queue, &pos);
+			enqueue(queue, &pos, queue_size);
 		}
 
 		if (check(data, maze, x_i + 1, y_j)) {
@@ -247,7 +278,7 @@ bool search(t_data *data, char **maze, int s_i, int s_j) {
 			pos.j = y_j; /* 右方向のマスのy_j座標 */
 
 			/* 右方向のマスを探索候補としてキューに格納*/
-			enqueue(&queue, &pos);
+			enqueue(queue, &pos, queue_size);
 		}
 	}
 
@@ -265,7 +296,7 @@ void	free_map(char **map, int rows)
 }
 
 
-void	bfs_search(t_data *data, char **bfs, int y, int	x)
+void	bfs_search(t_data *data, char **bfs, int x, int	y)
 {
 	for (int j=0; j < y; j++){
 	printf("%d行目 = {", j);
@@ -275,17 +306,30 @@ void	bfs_search(t_data *data, char **bfs, int y, int	x)
 	printf("}\n");
 	}
 
-	initQueue(&queue);
+	QUEUE_T queue;
+	initQueue(&queue, x, y);
 
 	/* ゴールの位置を設定 */
 	// maze[3][3] = GOAL;
 
 	/* スタート位置を(1,3)として開始 */
-	if (search(bfs, data->player.y, data->player.x)) {
+	if (search(data, &queue, bfs, data->player.x, data->player.y)) {
 		printf("ゴールが見つかりました！\n");
 	} else {
 		printf("ゴールが見つかりません！\n");
 	}
+	if (queue.data) {
+		free(queue.data);
+		queue.data = NULL;
+	}
+	for (int j=0; j < y; j++){
+	printf("%d行目 = {", j);
+	for (int i=0; i < x; i++){
+		printf("%c, ", bfs[j][i]);
+	}
+	printf("}\n");
+	}
+
 }
 
 void	img_disply(int *components, void	*img, int x, int y, char s)
@@ -305,7 +349,7 @@ void	read_map(t_data *data, char *filename)
 	bfs = (char **)malloc(sizeof(char *) * MAX_MAP);
 	if (!bfs)
 		exit(1);
-
+	data->coll_con= 0;
 	j = 0;
 	data->fd = open(filename, O_RDONLY);
 	if (data->fd < 0)
@@ -324,7 +368,10 @@ void	read_map(t_data *data, char *filename)
 			else if (line[i] == '1')
 				img_disply(&data->img->wall, data->img->wall_img, j, i, line[i]);
 			else if (line[i] == 'C')
+			{
 				img_disply(&data->img->collects, data->img->col_img, j, i, line[i]);
+				data->coll_con++;
+			}
 			else if (line[i] == 'E')
 				img_disply(&data->img->exit, data->img->exit_img, j, i, line[i]);
 			else if (line[i] == 'P')
@@ -350,7 +397,7 @@ void	read_map(t_data *data, char *filename)
 	data->y_column = j;
 	// free(line);
 
-	bfs_search(data, bfs, j, data->x_row);
+	bfs_search(data, bfs,  data->x_row, j);
 
 	free_map(bfs, j);
 }
